544093e3cdfb9bcfa4e26d2d2bfc490d0737a901
Resul Genç <rresulgenc@gmail.com>
2024-06-15 00:14:50 +0300

    changed

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Test V5, World!");
-//334
-Console.ReadLine();
+while (true)
+{
+    // Her döngüde bir şeyler yapabilirsiniz
+    // Örneğin, bir mesaj yazdırabilir veya kullanıcıdan bir giriş bekleyebilirsiniz
+    Console.WriteLine("Program hala çalışıyor. Çıkmak için 'q' tuşuna basın.");
+
+    // Kullanıcı 'q' tuşuna basarsa döngüyü kırıyoruz
+    if (Console.ReadLine() == "q")
+        break;
+}
 
